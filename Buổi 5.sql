cadf41e605a4af809ec601df3a46b3a8887d2038
tuantitus <nguyenvantuan.dhkh.k36@gmail.com>
2024-07-09 13:43:38 +0700

    Update Buổi 5.sql

--- Buổi 5.sql
+++ Buổi 5.sql
@@ -1,3 +1,9 @@
 -- Bài 1:
 SELECT DISTINCT CITY FROM STATION
 WHERE ID%2=0
+--Bài 2:
+Select (count (city)- count (distinct city)) as sum from station;
+--Bài 4:
+SELECT 
+ROUND(CAST(SUM(item_count*order_occurrences) / SUM(order_occurrences)DECEMAL),1) AS gia_tri
+FROM items_per_order
